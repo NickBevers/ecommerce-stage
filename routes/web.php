202705a13295e3b4b72df6525e88077d71910908
Ismail <elghamri.ismail@gmail.com>
2023-02-20 16:21:07 +0100

    feat(web.php): created routes for overview

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/overview', function () {
+    return Inertia::render('Dashboard/Overview');
+})->middleware(['auth', 'verified'])->name('overview');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
