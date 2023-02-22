ca4be5fb228a7057485cc04d92a76cf4df310734
Nick Bevers <nick.bevers@artcoresociety.com>
2023-02-22 13:25:48 +0100

    fix: change WhishlistController to WishlistController

--- app/Http/Controllers/WishlistsController.php
+++ app/Http/Controllers/WishlistsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Wishlist;
 use Illuminate\Http\Request;
 
-class WhishlistsController extends Controller
+class WishlistsController extends Controller
 {
     public function index()
     {
