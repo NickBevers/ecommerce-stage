99e63b0479a5ceb74a9d8b3efd890c8c69328f2f
Nick Bevers <nick.bevers@artcoresociety.com>
2023-02-22 08:49:12 +0100

    feat: add Cart class with migration, seeder, factory and controller

--- database/migrations/2023_02_20_094031_create_orders_table.php
+++ database/migrations/2023_02_20_094031_create_orders_table.php
@@ -11,14 +11,13 @@ return new class extends Migration {
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('shipping_address_id')->constrained('addresses');
-            $table->json('products');
-            $table->integer('price');
+            $table->timestamp('order_date');
+            $table->String('order_status');
+            $table->timestamp('preferred_delivery_date')->nullable();
             $table->string('payment_method');
-            $table->string('status');
             $table->string('tracking_number')->nullable();
             $table->string('tracking_url')->nullable();
             $table->timestamp('shipped_at')->nullable();
-            $table->timestamps();
         });
     }
 
--- database/migrations/2023_02_21_090820_create_addresses_table.php
+++ database/migrations/2023_02_21_090820_create_addresses_table.php
@@ -7,20 +7,22 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('shipping_addresses', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->string('address_line1');
             $table->string('address_line2')->nullable();
             $table->string('city');
             $table->string('postal_code');
+            $table->String('address_type');
             $table->string('country');
+            $table->String('btw_number')->nullable();
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('shipping_addresses');
+        Schema::dropIfExists('addresses');
     }
 };
