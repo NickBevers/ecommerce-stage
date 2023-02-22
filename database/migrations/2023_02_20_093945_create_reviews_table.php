c4c55b6c39565093417cc58978bf24cc5aac2d8e
Nick Bevers <nick.bevers@artcoresociety.com>
2023-02-22 09:18:53 +0100

    feat: add columns to migration for reviews table

--- database/migrations/2023_02_20_093945_create_reviews_table.php
+++ database/migrations/2023_02_20_093945_create_reviews_table.php
@@ -9,10 +9,15 @@ return new class extends Migration {
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-
-
-
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title');
+            $table->text('body');
+            $table->integer('score');
+            $table->boolean('approved')->default(false);
+            $table->integer('useful_count')->default(0);
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
