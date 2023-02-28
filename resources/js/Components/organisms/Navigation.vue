0c87834871458fb3e1ceed534302be25e19e8d23
Ismail Elg <elghamri.ismail@gmail.com>
2023-02-28 15:17:42 +0100

    feat(Welcome): added navigation placeholder

--- resources/js/Components/organisms/Navigation.vue
+++ resources/js/Components/organisms/Navigation.vue
@@ -2,21 +2,36 @@
 import ApplicationLogo from '@/Components/atoms/ApplicationLogo.vue';
 import { ref } from 'vue';
 
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+
 const showSubMenu = ref(false);
+
+function showMenu(type) {
+    if (type === 'show') {
+        showSubMenu.value = true;
+    } else {
+        showSubMenu.value = false;
+    }
+}
 </script>
 <template>
     
-<nav class="bg-white w-full z-20 border-gray-200 dark:border-gray-600 absolute top-0 left-0 dark:bg-gray-900">
+<nav  @mouseleave="showMenu('hide')"  class="bg-white w-full z-20 border-gray-200 dark:border-gray-600 absolute top-0 left-0 dark:bg-gray-900">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5 relative">
         <a href="https://flowbite.com" class="flex items-center">
             <ApplicationLogo class="w-8 h-8 mr-2" />
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Logo Name</span>
         </a>
-        <button type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
+        <button @click="showSubMenu!=showSubMenu" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
         </button>
-        <div  class="items-center justify-between hidden w-full md:flex md:w-auto md:order-2">
+        <div  @mouseenter="showMenu('hide')" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-2">
             <ul class="flex flex-row gap-4">
                 <li><svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg></li>
                 <li><svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/></svg></li>
@@ -26,7 +41,7 @@ const showSubMenu = ref(false);
         </div>
         <div id="mega-menu-full" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
             <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li @mouseenter="showSubMenu != showSubMenu">
+                <li @mouseenter="showMenu('show')">
                     <button   id="mega-menu-full-dropdown-button" class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-700 rounded md:w-auto hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Clothing <svg class="w-5 h-5 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
                 </li>
                 <li>
@@ -41,7 +56,7 @@ const showSubMenu = ref(false);
             </ul>
         </div>
     </div>
-    <div id="mega-menu-full-dropdown" v-if="showSubMenu" class="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
+    <div id="mega-menu-full-dropdown"  v-if="showSubMenu" class="-mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600">
         <div class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
             <ul>
                 <li>
@@ -84,9 +99,6 @@ const showSubMenu = ref(false);
                 </li>
             </ul>
         </div>
-    </div>
-    <div>
-        
     </div>
 </nav>
 
