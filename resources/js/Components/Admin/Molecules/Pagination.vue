1676c004166d900615b04831f9f88e78ec247e0a
Ismail Elg <elghamri.ismail@gmail.com>
2023-03-06 16:16:37 +0100

    fix(Pagination): changed link to inertia

--- resources/js/Components/Admin/Molecules/Pagination.vue
+++ resources/js/Components/Admin/Molecules/Pagination.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { defineProps, onMounted } from 'vue'
+import {Link} from '@inertiajs/vue3';
 
 let props = defineProps({
     links: Array,
@@ -15,9 +16,11 @@ onMounted(() => {
         <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-end">
             <div>
                 <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                    <div v-for="link in props.links"> <a v-if="link" :href="link.url"
+                    <div v-for="link in props.links"> 
+                        <Link v-if="link" :href="link.url"
                             :class="{ 'bg-indigo-600': props.links.active }"
-                            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">{{ link.label }}</a>
+                            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">{{ link.label }}
+                        </Link>
                     </div>
                 </nav>
             </div>
