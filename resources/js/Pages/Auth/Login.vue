c9b75f1f540dd97335c8a31561757a5b0a2edd44
Ismail Elg <elghamri.ismail@gmail.com>
2023-03-06 13:21:36 +0100

    feat(Login): changed login screen

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Checkbox from "@/Components/Admin/Atoms/Checkbox.vue";
+import ApplicationLogo from "@/Components/Customer/Atoms/ApplicationLogo.vue";
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import InputError from "@/Components/Admin/Atoms/InputError.vue";
 import InputLabel from "@/Components/Admin/Atoms/InputLabel.vue";
@@ -27,7 +27,6 @@ const submit = () => {
 
 <template>
   <GuestLayout>
-
     <Head title="Log in" />
     <h1 class="text-xl mb-8 mt-4 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
       Sign in to your account
@@ -75,5 +74,61 @@ const submit = () => {
         </Link>
       </div>
     </form>
+    
   </GuestLayout>
+  <div class="flex min-h-full">
+    <div class="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+      <div class="mx-auto w-full max-w-sm lg:w-96">
+        <div>
+          <ApplicationLogo class="h-12 w-auto" />
+          <h2 class="mt-6 text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+        </div>
+
+        <div class="mt-8">
+          <div class="mt-6">
+            <form @submit.prevent="submit" class="space-y-6">
+              <div>
+                <InputLabel for="email" value="Email address" />
+                <div class="mt-2">
+                  <TextInput id="email" type="email" v-model="form.email" required autofocus autocomplete="username" placeholder="name@mail.com" />
+                  <InputError class="mt-2" :message="form.errors.email" />
+                </div>
+              </div>
+
+              <div class="space-y-1">
+                <InputLabel for="password" value="Password" />
+                <div class="mt-2">
+                  <TextInput id="password" type="password" v-model="form.password" required autocomplete="current-password" placeholder="••••••••" />
+                  <InputError class="mt-2" :message="form.errors.password" />
+                </div>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <div class="text-sm">
+                  <Link v-if="canResetPassword" :href="route('password.request')"
+                    class="font-medium text-indigo-600 hover:text-indigo-500">
+                        Forgot your password?
+                  </Link>
+                </div>
+              </div>
+
+              <div>
+                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <div class="text-sm">
+                  Don't have an account yet? 
+                  <Link :href="route('register')" class="font-medium text-indigo-600 hover:text-indigo-500">Sign up</Link>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="relative hidden w-0 flex-1 lg:block">
+      <img class="absolute inset-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80" alt="" />
+    </div>
+  </div>
 </template>
