97f0a1faee401bfb03232a70e0364948d416b848
Ismail Elg <elghamri.ismail@gmail.com>
2023-03-07 10:49:14 +0100

    feat(Products): seperate folder for product routes

---
+++ resources/js/Pages/Admin/Products/Create.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { defineProps } from 'vue'
+import { Head, Link } from '@inertiajs/vue3';
+
+</script>
+<template>
+    <Head title="Products" />
+   <AuthenticatedLayout>
+        <div class="mt-8">
+               <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"> 
+                            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-xl font-semibold leading-6 text-gray-900">Add product</h1>
+                </div>
+                <div class="sm:mt-0 sm:ml-16 sm:flex-none">
+               
+                </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
