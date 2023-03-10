8e3271d0eb6e902a8197a45dc94d9aa7c158f363
Nick Bevers <nick.bevers@artcoresociety.com>
2023-03-10 08:58:58 +0100

    feat: add promotion routes and basic functionality to create and update a promotion

--- app/Http/Controllers/Admin/PromoController.php
+++ app/Http/Controllers/Admin/PromoController.php
@@ -6,21 +6,42 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Product\ProductController;
 use App\Models\Promo;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class PromoController extends Controller
 {
     public function index()
     {
-        
+        return Inertia::render('Admin/Promos/Index');
     }
 
     public function create()
     {
+        return Inertia::render('Admin/Promos/Create');
     }
 
     public function store(Request $request)
     {
-        ProductController::setPromotionStatus($request->id);
+        // create a new promo after validating the request
+        $request->validate([
+            'sku_id' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'new_price' => 'required',
+            'extra_info' => 'String,max:1000',
+        ]);
+
+        Promo::create([
+            'sku_id' => $request->sku_id,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
+            'new_price' => $request->new_price,
+            'extra_info' => $request->extra_info,
+        ]);
+
+        return redirect()
+            ->route('products.index')
+            ->with('success', 'Promo created successfully.');
     }
 
     public function show(Promo $promo)
@@ -29,13 +50,36 @@ class PromoController extends Controller
 
     public function edit(Promo $promo)
     {
+        return Inertia::render('Admin/Promos/Edit', [
+            'promo' => $promo,
+        ]);
     }
 
     public function update(Request $request, Promo $promo)
     {
+        $request->validate([
+            'sku_id' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'new_price' => 'required',
+            'extra_info' => 'String,max:1000',
+        ]);
+
+        //patch the promo
+        $promo->fill($request->all());
+        $promo->save();
+
+        return redirect()
+            ->route('products.index')
+            ->with('success', 'Promo updated successfully.');
     }
 
     public function destroy(Promo $promo)
     {
+        $promo->delete();
+
+        return redirect()
+            ->route('products.index')
+            ->with('success', 'Promo deleted successfully.');
     }
 }
