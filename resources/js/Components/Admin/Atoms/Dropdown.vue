6d842133b38be14d3599c3ddfb568068185e3092
Ismail Elg <elghamri.ismail@gmail.com>
2023-03-10 16:53:19 +0100

    refactor: format

--- resources/js/Components/Admin/Atoms/Dropdown.vue
+++ resources/js/Components/Admin/Atoms/Dropdown.vue
@@ -3,8 +3,7 @@
     <div class="relative mt-2">
       <ListboxButton
         class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        :class="{ 'opacity-50': isDisabled }"
-        >
+        :class="{ 'opacity-50': isDisabled }">
         <span v-if="!isDisabled" class="block truncate">{{ selected.name ? selected.name : selected }}</span>
         <span v-else class="block truncate">No options available</span>
         <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
@@ -12,27 +11,20 @@
         </span>
       </ListboxButton>
 
-      <transition
-        leave-active-class="transition ease-in duration-100"
-        leave-from-class="opacity-100"
-        leave-to-class="opacity-0"
-      >
+      <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
+        leave-to-class="opacity-0">
         <ListboxOptions
-          class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-        >
-          <ListboxOption
-            as="template"
-            v-for="item in props.items"
-            :key="item"
-            :value="item.item ? item.item : item"
-            v-slot="{ active, selected }"
-          >
-            <li :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-8 pr-4']">
+          class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+          <ListboxOption as="template" v-for="item in props.items" :key="item" :value="item.item ? item.item : item"
+            v-slot="{ active, selected }">
+            <li
+              :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-8 pr-4']">
               <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">
                 {{ item.name ? item.name : item.item ? item.item : item }}
               </span>
 
-              <span v-if="selected" :class="[active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 left-0 flex items-center pl-1.5']">
+              <span v-if="selected"
+                :class="[active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 left-0 flex items-center pl-1.5']">
                 <CheckIcon class="h-5 w-5" aria-hidden="true" />
               </span>
             </li>
