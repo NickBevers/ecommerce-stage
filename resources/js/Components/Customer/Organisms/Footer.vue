0b167226a08bcce69528514e8a64a69cccf8e388
Ismail Elg <elghamri.ismail@gmail.com>
2023-03-13 14:47:55 +0100

    refactor(Footer): removed email subscribtion

--- resources/js/Components/Customer/Organisms/Footer.vue
+++ resources/js/Components/Customer/Organisms/Footer.vue
@@ -16,7 +16,7 @@
     <footer class="bg-white" aria-labelledby="footer-heading">
       <h2 id="footer-heading" class="sr-only">Footer</h2>
       <div class="mx-auto max-w-7xl px-6 pb-8 pt-20 sm:pt-24 lg:px-8 lg:pt-32">
-        <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+        <div class="xl:grid xl:grid-cols-2 xl:gap-8">
           <div class="grid grid-cols-2 gap-8 xl:col-span-2">
             <div class="md:grid md:grid-cols-2 md:gap-8">
               <div>
@@ -54,17 +54,6 @@
                 </ul>
               </div>
             </div>
-          </div>
-          <div class="mt-10 xl:mt-0">
-            <h3 class="text-sm font-semibold leading-6 text-gray-900">Subscribe to our newsletter</h3>
-            <p class="mt-2 text-sm leading-6 text-gray-600">The latest news, articles, and resources, sent to your inbox weekly.</p>
-            <form class="mt-6 sm:flex sm:max-w-md">
-              <label for="email-address" class="sr-only">Email address</label>
-              <input type="email" name="email-address" id="email-address" autocomplete="email" required="" class="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:w-64 sm:text-sm sm:leading-6 xl:w-full" placeholder="Enter your email" />
-              <div class="mt-4 sm:mt-0 sm:ml-4 sm:flex-shrink-0">
-                <button type="submit" class="flex w-full items-center justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Subscribe</button>
-              </div>
-            </form>
           </div>
         </div>
         <div class="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
