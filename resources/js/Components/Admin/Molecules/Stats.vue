0a9e0edbc72e3588d3ac3e0b8c3234ab164222a4
Ismail Elg <elghamri.ismail@gmail.com>
2023-03-14 14:57:05 +0100

    refactor(Stats): changed stats

--- resources/js/Components/Admin/Molecules/Stats.vue
+++ resources/js/Components/Admin/Molecules/Stats.vue
@@ -26,11 +26,11 @@
   
   <script setup>
   import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/vue/20/solid'
-  import { CursorArrowRaysIcon, EnvelopeOpenIcon, UsersIcon } from '@heroicons/vue/24/outline'
+  import { CurrencyEuroIcon , ShoppingBagIcon , ShoppingCartIcon  } from '@heroicons/vue/24/outline'
   
   const stats = [
-    { id: 1, name: 'Total Subscribers', stat: '71,897', icon: UsersIcon, change: '122', changeType: 'increase' },
-    { id: 2, name: 'Avg. Open Rate', stat: '58.16%', icon: EnvelopeOpenIcon, change: '5.4%', changeType: 'increase' },
-    { id: 3, name: 'Avg. Click Rate', stat: '24.57%', icon: CursorArrowRaysIcon, change: '3.2%', changeType: 'decrease' },
+    { id: 1, name: 'Orders', stat: '0', icon: ShoppingBagIcon , change: '24%', changeType: 'increase' },
+    { id: 2, name: 'Sold Items', stat: '0', icon: ShoppingCartIcon , change: '24%', changeType: 'increase' },
+    { id: 3, name: 'Sales', stat: '€200', icon: CurrencyEuroIcon, change: '24%', changeType: 'decrease' },
   ]
   </script>
