d8cf11896b33b72deb0bd8f59017012f476391c4
Ismail Elg <elghamri.ismail@gmail.com>
2023-03-17 13:44:36 +0100

    refactor(ProductsList): 2 decimcals for price

--- resources/js/Components/Customer/Molecules/ProductsList.vue
+++ resources/js/Components/Customer/Molecules/ProductsList.vue
@@ -13,7 +13,7 @@
             </div>
             <div class="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
               <div aria-hidden="true" class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50" />
-              <p class="relative text-lg font-semibold text-white">€{{ product.price }}</p>
+              <p class="relative text-lg font-semibold text-white">€{{ product.price.toFixed(2) }}</p>
             </div>
             <div>
               <div class="absolute top-0 right-0 p-2 m-2">
