a08ae4693e2235a9682a14884b96ca88af097710
Ismail Elg <elghamri.ismail@gmail.com>
2023-03-17 14:56:34 +0100

    style: hover animation

--- resources/js/Components/Customer/Molecules/ProductsList.vue
+++ resources/js/Components/Customer/Molecules/ProductsList.vue
@@ -3,15 +3,16 @@
     <div class="mx-auto max-w-2xl py-6 px-4 sm:py-6 sm:px-6 lg:max-w-7xl lg:px-8">
       <div class="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
         <div v-for="product in props.skus" :key="product.id" class="mt-8">
-          <div class="relative">
-            <div class="relative h-72 w-full overflow-hidden rounded-lg">
-              <img src="https://tailwindui.com/img/ecommerce-images/product-page-03-related-product-01.jpg" alt="placeholder" class="h-full w-full object-cover object-center" />
+          <div class="relative imgScale">
+            <div class=" relative h-72 w-full overflow-hidden rounded-lg  duration-500 ">
+              <img src="https://tailwindui.com/img/ecommerce-images/product-page-03-related-product-01.jpg"
+                alt="placeholder" class="h-full w-full object-cover object-center" />
             </div>
             <div class="relative mt-4">
               <h3 class="text-sm font-medium text-gray-900">{{ product.product.title }}</h3>
               <p class="mt-1 text-sm text-gray-500">{{ product.product.description.slice(0, 30) }}...</p>
             </div>
-            <div class="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
+            <div class="grade absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
               <div aria-hidden="true" class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50" />
               <p class="relative text-lg font-semibold text-white">€{{ product.price.toFixed(2) }}</p>
             </div>
@@ -28,6 +29,25 @@
     </div>
   </div>
 </template>
+<style scoped>
+.imgScale img {
+  transition: all 0.5s ease;
+
+}
+
+.imgScale:hover img {
+  background-color: red;
+  transform: scale(1.1);
+}
+
+.imgScale .grade div {
+  transition: all 0.5s ease;
+}
+
+.imgScale:hover .grade div {
+  height: 6rem;
+}
+</style>
   
 <script setup>
 import { defineProps, onMounted } from 'vue'
@@ -39,6 +59,4 @@ const props = defineProps({
   skus: Object,
   links: Object,
 });
-
-
 </script>
