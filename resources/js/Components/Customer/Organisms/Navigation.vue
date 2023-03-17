8f30277ca8724e8728cebbf01fd4e587df1dc990
Ismail Elg <elghamri.ismail@gmail.com>
2023-03-17 13:39:23 +0100

    fix(Navigation): accidentally sliced 3rd category

--- resources/js/Components/Customer/Organisms/Navigation.vue
+++ resources/js/Components/Customer/Organisms/Navigation.vue
@@ -296,7 +296,7 @@ function selectCategory(category_id) {
 
 
                                                             <div>
-                                                                <div  v-for="(item, index) in category.subCategories.slice(3, 10)" :key="index"
+                                                                <div  v-for="(item, index) in category.subCategories.slice(2, 10)" :key="index"
                                                                 class="group relative">
                                                                 <div v-if="!item.preview" class="flex flex-row py-2">
                                                                     <Link :to="'/products/' + item.slug" :href="'/products/' + item.slug"
