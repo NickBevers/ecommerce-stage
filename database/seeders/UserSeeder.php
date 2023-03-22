9a3ac3c8b7a1d5ab0f7947cc4f49c04c4b55b628
Nick Bevers <nick.bevers@artcoresociety.com>
2023-03-22 10:42:26 +0100

    feat: add admin check as middleware

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,6 +9,25 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        User::factory()->count(10)->create();
+//        User::factory()->count(10)->create();
+        User::create(
+            [
+                'firstname' => 'Nick',
+                'lastname' => 'Bevers',
+                'email' => 'nick.bevers9@gmail.com',
+                'password' => bcrypt('test1234'),
+                'user_type' => 'admin',
+            ]
+        );
+
+        User::create(
+            [
+                'firstname' => 'Ismail',
+                'lastname' => 'El Ghamri',
+                'email' => 'ismail@gmail.com',
+                'password' => bcrypt('ismail123'),
+                'user_type' => 'admin',
+            ]
+        );
     }
 }
