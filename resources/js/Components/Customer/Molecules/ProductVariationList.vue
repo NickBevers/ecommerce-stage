28f5b9671d7fcbc9dc07b172935ac593968309a3
ismailElg <91196906+Ismail-Elg@users.noreply.github.com>
2023-03-24 14:27:23 +0100

    feat(Product): added variation list component

---
+++ resources/js/Components/Customer/Molecules/ProductVariationList.vue
@@ -0,0 +1,43 @@
+<template>
+        <div class="flex flex-wrap gap-2">
+            <div v-for="variation, index in props.variations" :key="variation.id" class="m-1 overflow-hidden rounded-md" :class="{'hidden': !showAll && index > 3 }">
+              <div v-if="!showAll" class="overflow-hidden">
+                <div class="h-32 w-20 inline hover:scale-125 duration-500 col-span-4 transition-all rounded-md object-cover">
+                <Link :href="`/product/${variation.sku}`">
+                  <img class="h-32 w-20  inline hover:scale-125 duration-500 transition-all rounded-md object-cover"
+                    :src="variation.product_images[0].image_link"
+                    alt="variation.name" />
+                </Link>
+              </div>
+              </div>
+              <div v-else>
+                <div class="h-32 w-20 inline hover:scale-125 duration-500 transition-all rounded-md object-cover">
+                  <Link :href="`/product/${variation.sku}`">
+                    <img class="h-32 w-20 inline hover:scale-125 duration-500 transition-all rounded-md object-cover"
+                    :src="variation.product_images[0].image_link"
+                      alt="variation.name" />
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div v-if="props.variations.length > 4 && !showAll" class="flex text-center justify-center content-center mt-1 object-center rounded-md">
+              <button class="h-32 w-20  border-2 border-indigo-300 rounded-md hover:border-indigo-600 text-indigo-300 hover:text-indigo-600 duration-500 transition-all" @click.prevent="showAll = !showAll">+{{props.variations.length - 4}}</button>
+            </div>
+                         
+          </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+  variations: {
+    type: Object,
+    required: true
+  }
+})
+
+
+let showAll = ref(false);
+
+</script>
--- resources/js/Pages/Customer/Product/Index.vue
+++ resources/js/Pages/Customer/Product/Index.vue
@@ -10,38 +10,12 @@
           <ProductStars/>
         </div>
 
-        <!-- Image gallery -->
         <ImageGallery :sku="props.sku" />
 
         <div class="mt-8 lg:col-span-5">
           <form>
             <div class="mb-6">
-            <div class="flex flex-wrap gap-2">
-            <div v-for="variation, index in props.variations" :key="variation.id" class="m-1 overflow-hidden rounded-md" :class="{'hidden': !showAll && index > 3 }">
-              <div v-if="!showAll" class="overflow-hidden">
-                <div class="h-32 w-20 inline hover:scale-125 duration-500 col-span-4 transition-all rounded-md object-cover">
-                <Link :href="`/product/${variation.sku}`">
-                  <img class="h-32 w-20  inline hover:scale-125 duration-500 transition-all rounded-md object-cover"
-                    :src="variation.product_images[0].image_link"
-                    alt="variation.name" />
-                </Link>
-              </div>
-              </div>
-              <div v-else>
-                <div class="h-32 w-20 inline hover:scale-125 duration-500 transition-all rounded-md object-cover">
-                  <Link :href="`/product/${variation.sku}`">
-                    <img class="h-32 w-20 inline hover:scale-125 duration-500 transition-all rounded-md object-cover"
-                    :src="variation.product_images[0].image_link"
-                      alt="variation.name" />
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div v-if="props.variations.length > 4 && !showAll" class="flex text-center justify-center content-center mt-1 object-center rounded-md">
-              <button class="h-32 w-20  border-2 border-indigo-300 rounded-md hover:border-indigo-600 text-indigo-300 hover:text-indigo-600 duration-500 transition-all" @click.prevent="showAll = !showAll">+{{props.variations.length - 4}}</button>
-            </div>
-                         
-          </div>
+              <ProductVariationList :variations="props.variations" />
             </div>
        
             <!-- Color picker -->
@@ -128,6 +102,7 @@
 import { defineProps, onMounted, computed, ref, reactive } from 'vue'
 import GuestLayout from '@/Layouts/GuestLayout.vue'
 import ProductReviews from '@/Components/Customer/Molecules/ProductReviews.vue';
+import ProductVariationList from '@/Components/Customer/Molecules/ProductVariationList.vue';
 import ProductStars from '@/Components/Customer/Molecules/ProductStars.vue';
 import ImageGallery from '@/Components/Customer/Molecules/ImageGallery.vue';
 import ProductDetails from '@/Components/Customer/Molecules/ProductDetails.vue';
@@ -141,11 +116,5 @@ const props = defineProps({
   colors: Object,
   material: Object,
 });
-
-let showAll = ref(false);
-
-
-
-console.log(props.sku)
 
 </script>
