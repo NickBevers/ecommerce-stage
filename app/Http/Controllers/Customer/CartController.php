374bb958bae709617a430ed1a856ef16f77a8ff5
ismailElg <91196906+Ismail-Elg@users.noreply.github.com>
2023-03-24 11:52:25 +0100

    feat(ShoppingBag): added shoppingbag page

--- app/Http/Controllers/Customer/CartController.php
+++ app/Http/Controllers/Customer/CartController.php
@@ -11,7 +11,7 @@ class CartController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Customer/Cart/Index', [
+        return Inertia::render('Customer/Checkout/ShoppingBag', [
             'cart' => $this->getProductsPerUser(),
         ]);
     }
--- resources/js/Components/Customer/Organisms/Navigation.vue
+++ resources/js/Components/Customer/Organisms/Navigation.vue
@@ -362,14 +362,14 @@ function selectCategory(category_id) {
                                     </div>
                                     <!-- Cart -->
                                     <div class="ml-4 flow-root">
-                                        <a href="#" class="group -m-2 flex items-center p-2">
+                                        <Link href="/cart" class="group -m-2 flex items-center p-2">
                                             <ShoppingBagIcon
                                                 class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
                                                 aria-hidden="true" />
                                             <span
                                                 class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                                             <span class="sr-only">items in cart, view bag</span>
-                                        </a>
+                                        </Link>
                                     </div>
                                 </div>
                             </div>
