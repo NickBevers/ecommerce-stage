838e9c03a7792446b001b2b8b454ccfe8cf01e6d
Ismail Elg <elghamri.ismail@gmail.com>
2023-03-27 10:10:03 +0200

    style(ImageGallery): fixed height

--- resources/js/Components/Customer/Molecules/ImageGallery.vue
+++ resources/js/Components/Customer/Molecules/ImageGallery.vue
@@ -2,7 +2,7 @@
         <div class="mt-8 lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0">
           <h2 class="sr-only">Images</h2>
           <div class="flex flex-row gap-4 sticky top-0">
-           <div class="flex flex-col w-1/3 gap-4 h-[38rem] overflow-y-scroll no-scrollbar relative">
+           <div class="flex flex-col w-1/3 gap-4 h-[42rem] overflow-y-scroll no-scrollbar relative">
             <div v-for="image in props.sku.product_images">
             <div  class="rounded-lg overflow-hidden h-60 w-full" @mouseenter="imagePreview = image.image_link">
               <img
@@ -13,7 +13,7 @@
           </div>
             
            </div>
-          <div class="flex flex-col w-2/2 h-fit w-full h-[38rem] rounded-md overflow-hidden ">
+          <div class="flex flex-col w-2/2 w-full h-[42rem] rounded-md overflow-hidden ">
                 <img :src="imagePreview" class="h-full w-full object-cover object-center">
           </div>
         </div>
