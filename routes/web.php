0d89cc81fc9659a91b00fd36aea0264689051420
Ismail Elg <elghamri.ismail@gmail.com>
2023-04-04 15:32:45 +0200

    fix(CartController): changed the component that gets rendered

--- routes/web.php
+++ routes/web.php
@@ -98,7 +98,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/cart/{sku}', [CartController::class, 'update'])->name('cart.update');
     Route::delete('/cart/{sku}', [CartController::class, 'destroy'])->name('cart.destroy');
 
-    Route::get('/checkout', function () {  return Inertia::render('Customer/Checkout/Index'); })->name('checkout.index');
+    // Route::get('/checkout', function () {  return Inertia::render('Customer/Checkout/Index'); })->name('checkout.index');
 
     // Wishlist Routes
     Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist.index');
