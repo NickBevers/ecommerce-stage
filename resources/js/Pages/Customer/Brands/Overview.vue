a157706df2d970633d7d2c3bec8a1fcbc113a5a5
Ismail Elg <elghamri.ismail@gmail.com>
2023-04-04 12:29:10 +0200

    feat(Brands): show products depending on the selected brand

--- resources/js/Pages/Customer/Brands/Overview.vue
+++ resources/js/Pages/Customer/Brands/Overview.vue
@@ -6,19 +6,16 @@ import { ProductsFilter, ProductsList } from '@/Components/Customer'
 const props = defineProps({
     brand: Object,
     skus: Object,
-    sku: Object,
 })
 
 onMounted(() => {
     console.log(props.brand)
     console.log(props.skus)
-    console.log(props.sku)
 })
 </script>
 <template>
     <GuestLayout>
         <ProductsFilter :title="props.brand.name" />
-        {{ skus.data }}
-        <!-- <ProductsList :skus="skus.data" :links="skus.links" /> -->
+        <ProductsList :skus="skus.data" :links="skus.links" />
     </GuestLayout>
 </template>
