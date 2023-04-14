2179dedab110a18c7ddff097e330afc19779d42b
ismailElg <91196906+Ismail-Elg@users.noreply.github.com>
2023-04-14 12:49:11 +0200

    feat: added order detail route

--- resources/js/Components/Admin/Organisms/OrderTable.vue
+++ resources/js/Components/Admin/Organisms/OrderTable.vue
@@ -35,17 +35,17 @@ onMounted(() => {
                                     </th>
                                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Amount
                                     </th>
-                                    <th scope="col" class="px-3 py-3.5 text-center   text-sm font-semibold text-gray-900">
+                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                                         Status
                                     </th>
-                                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Action
+                                    <th scope="col" class="px-3 py-3.5 text-right text-sm font-semibold text-gray-900">Action
                                     </th>
                                 </tr>
                             </thead>
                             <tbody class="divide-y divide-gray-200 bg-white">
                                 <tr v-for="order in orders" :key="order.id">
                                     <td class="whitespace-nowrap py-2 pr-3 text-sm sm:pl-0t">
-                                        <div class="flex items-left ">
+                                        <div class="flex items-left">
                                             <div class="font-medium text-gray-900">#{{ order.order.id }}</div>
                                         </div>
                                     </td>
@@ -76,16 +76,16 @@ onMounted(() => {
                                         €{{
                                             order.order.total_price.toFixed(2) }}
                                     </td>
-                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center">
+                                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-left">
                                         <span
-                                            class="inline-flex items-center rounded-full bg-gray-100 px-3 py-0.5 text-sm font-medium text-gray-800">{{
+                                            class="inline-flex items-left rounded-full bg-gray-100 px-3 py-0.5 text-sm font-medium text-gray-800">{{
                                                 order.order.order_status }}</span>
                                     </td>
                                     <td
                                         class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right justify-end text-sm font-medium sm:pr-0 flex gap-2">
-                                        <Link href="/admin/orders"
+                                        <Link :href="route('admin.orders.show', order.order.id)"
                                             class="rounded-full bg-indigo-600 p-2 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                        <EyeIcon class="h-3 w-3" aria-hidden="true" />
+                                            <EyeIcon class="h-3 w-3" aria-hidden="true" />
                                         </Link>
                                         <button type="button" @click="open = true;"
                                             class="rounded-full bg-red-600 p-2 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
---
+++ resources/js/Pages/Admin/Orders/Detail.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { OrderTable } from '@/Components/Admin';
+import { Head, Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    order: Object,
+});
+
+console.log(props.order)
+</script>
+<template>
+    <Head title="Products" />
+    <AuthenticatedLayout>
+        <div class="mt-8">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="sm:flex sm:items-center">
+                    <div class="sm:flex-auto">
+                        <h1 class="text-xl font-semibold leading-6 text-gray-900">{{ props.order }}</h1>
+                    </div>
+                </div>
+           
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
