d8cf86fbedb477df4fc4f3da0046f8d74d817a7b
Ismail Elg <elghamri.ismail@gmail.com>
2023-05-02 13:58:03 +0200

    feat(Home) added trending products

--- resources/js/Components/Customer/Organisms/Trending.vue
+++ resources/js/Components/Customer/Organisms/Trending.vue
@@ -1,51 +1,13 @@
 
 <script setup>
+import { Link } from '@inertiajs/vue3';
 
 const props = defineProps({
     recentOrderedProducts: Array,
     recentProducts: Array,
 });
 
-console.log(props);
-
-const products = [
-    {
-        id: 1,
-        name: 'Leather Long Wallet',
-        color: 'Natural',
-        price: '$75',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg',
-        imageAlt: 'Hand stitched, orange leather long wallet.',
-    },
-    {
-        id: 2,
-        name: 'Leather Travel Pouch',
-        color: 'Natural',
-        price: '$45',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-03.jpg',
-        imageAlt: 'Hand stitched, orange leather travel pouch.',
-    },
-    {
-        id: 3,
-        name: 'Leather Travel Pouch',
-        color: 'Natural',
-        price: '$45',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-04.jpg',
-        imageAlt: 'Hand stitched, orange leather travel pouch.',
-    },
-    {
-        id: 4,
-        name: 'Leather Travel Pouch',
-        color: 'Natural',
-        price: '$45',
-        href: '#',
-        imageSrc: 'https://images.unsplash.com/photo-1434389677669-e08b4cac3105?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=705&q=80',
-        imageAlt: 'Hand stitched, orange leather travel pouch.',
-    },
-]
+console.log(props.recentOrderedProducts, props.recentProducts)
 </script>
 <template>
     <div class="bg-white">
@@ -54,19 +16,19 @@ const products = [
                 <h2 class="text-2xl font-bold tracking-tight text-gray-900">Trending products</h2>
             </div>
             <div class="mt-6 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8">
-                <div v-for="product in products" :key="product.id" class="group relative">
+                <div v-for="product in (recentOrderedProducts.length > 0 ? recentOrderedProducts : recentProducts)"
+                    :key="product.id" class="group relative">
                     <div class="h-56 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-72 xl:h-80">
-                        <img :src="product.imageSrc" :alt="product.imageAlt"
+                        <img :src="product.product_images[0].image_link" :alt="product.product_images[0].alt"
                             class="h-full w-full object-cover object-center" />
                     </div>
                     <h3 class="mt-4 text-sm text-gray-700">
-                        <a :href="product.href">
-                            <span class="absolute inset-0" />
-                            {{ product.name }}
-                        </a>
+                        <Link :to="'/product/' + product.sku" :href="'/product/' + product.sku">
+                        <span class="absolute inset-0" />
+                        {{ product.product.title }}
+                        </Link>
                     </h3>
-                    <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
-                    <p class="mt-1 text-sm font-medium text-gray-900">{{ product.price }}</p>
+                    <p class="mt-1 text-sm font-medium text-gray-900">€{{ product.price_incl_vat }}</p>
                 </div>
             </div>
         </div>
